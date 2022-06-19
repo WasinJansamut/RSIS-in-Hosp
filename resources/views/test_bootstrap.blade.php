84f880cf622ed983f63e8079693ac426219708ff
Wasin Jansamut <wasinjansamut@gmail.com>
2022-06-19 22:29:00 +0700

    create laravel 8 + Bootstrap 5

---
+++ resources/views/test_bootstrap.blade.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boostrap 5</title>
+ <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
+</head>
+<body>
+    <div class="container">
+        <div class="alert alert-success mt-5" role="alert">
+            Boostrap 5 is working!
+        </div>    
+    </div>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
